e8986771dfd848a10cdb3da3d2a2b756ea41ae29
AYUSH UJJWAL <42496674+ayush1612@users.noreply.github.com>
2018-12-12 11:06:20 +0530

    Update ISE_department.java

--- ISE_Faculty_Department/src/ISE/ISE_department.java
+++ ISE_Faculty_Department/src/ISE/ISE_department.java
@@ -59,3 +59,10 @@ public class ISE_department extends Faculty implements Department{
         }
         }
     }
+      public static void main(String args[])
+    {
+        ISE_department D=new ISE_department();
+        D.readData();
+        D.printData();
+    }
+}
