96febee6c2af429e6d15c80e84587b6e8751823c
AYUSH UJJWAL <42496674+ayush1612@users.noreply.github.com>
2018-12-14 00:43:19 +0530

    Main Class

---
+++ ISE_Faculty_Department/src/MainClass.java
@@ -0,0 +1,10 @@
+package Default;
+import ISE.*;
+public class MainClass {
+    public static void main(String args[])
+    {
+        ISE_department D=new ISE_department();
+        D.readData();
+        D.printData();
+    }
+}
