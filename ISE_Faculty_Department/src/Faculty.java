591c4c5161f6f0b8b64f3b44660e6eeae31d0d93
AYUSH UJJWAL <42496674+ayush1612@users.noreply.github.com>
2018-12-14 00:42:17 +0530

    Updated Faculty

--- ISE_Faculty_Department/src/Faculty.java
+++ ISE_Faculty_Department/src/Faculty.java
@@ -1,18 +1,20 @@
+package Default;
+
 public class Faculty {
-     public String name;
-     public String designation;
-     public int age;
-     public int years_of_experience;
-     public int joining_date;
-     public int sub_hand;
- public Faculty()
-     {
-         name="";
-         designation="";
-         age=0;
-         years_of_experience=0;
-         joining_date=0;
-         sub_hand=0;
-     }
-     
+    public String name;
+    public String designation;
+    public int age;
+    public int years_of_experience;
+    public int joining_date;
+    public int sub_hand;
+public Faculty()
+    {
+        name="";
+        designation="";
+        age=0;
+        years_of_experience=0;
+        joining_date=0;
+        sub_hand=0;
+    }
+    
 }
